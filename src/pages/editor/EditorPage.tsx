f1e47f32740e83ef6cac7a76665d57abede03309
youjin-hong <asdfghjk1052@naver.com>
2026-03-31 12:03:10 +0900

    [ Feat | ✨ ] EditorPage 구현
    Toolbar, Canvas, StatusBar를 조합한 에디터 페이지 레이아웃 구성

---
+++ src/pages/editor/EditorPage.tsx
@@ -0,0 +1,29 @@
+import { useEditorStore } from '@/app/providers/editorStore';
+import { useKeyboardShortcuts } from '@/features/history';
+import { Toolbar } from '@/widgets/toolbar';
+import { Canvas } from '@/widgets/canvas';
+import { StatusBar } from '@/widgets/status-bar';
+
+export const EditorPage = (): JSX.Element => {
+  const setMode = useEditorStore((s) => s.setMode);
+  const undo = useEditorStore((s) => s.undo);
+  const redo = useEditorStore((s) => s.redo);
+  const completePolygon = useEditorStore((s) => s.completePolygon);
+  const cancelCurrentAction = useEditorStore((s) => s.cancelCurrentAction);
+
+  useKeyboardShortcuts({
+    setMode,
+    undo,
+    redo,
+    completePolygon,
+    cancelCurrentAction,
+  });
+
+  return (
+    <div className="w-screen h-screen bg-editor-bg flex overflow-hidden">
+      <Toolbar />
+      <Canvas />
+      <StatusBar />
+    </div>
+  );
+};
